835d0a035217d96980a0b82739e4d7d7be6c29f2
francis1009 <francism56789@gmail.com>
2025-04-22 02:46:14 +0800

    Clean up client template

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import TodoTable from "@/components/home/TodoTable.vue";
 </script>
 
 <template>
-	<main>
-		<TheWelcome />
-	</main>
+	<div class="hidden flex-1 flex-col space-y-8 p-8 md:flex">
+		<div class="flex items-center justify-center space-y-2">
+			<div>
+				<h2 class="text-2xl font-bold tracking-tight">Welcome!</h2>
+				<p class="text-muted-foreground mb-8">
+					Here is a list of all your tasks. You can add, edit, and delete tasks
+					as needed.
+				</p>
+				<TodoTable />
+			</div>
+		</div>
+	</div>
 </template>
